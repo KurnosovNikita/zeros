6745192d39fad75f70df95cb132db56e620103fd
KurnosovNikita <Kurnosov-nikita@mail.ru>
2018-02-25 03:55:53 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
+    var result = 0;
+    while (number >= 5){
+        number = ~~(number / 5);
+        result = result + number;
+    }
+    return result;
   // your implementation
 }
